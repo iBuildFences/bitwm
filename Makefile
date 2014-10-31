2e8d158936f94d77a43e0916e854aa37951f8f23
Mason Orsagh <masono@frontiernet.net>
2014-10-31 12:19:29 -0400

    Initial wm test commit. includes code to exec dmenu on ctrl+r.

---
+++ Makefile
@@ -0,0 +1,2 @@
+wm : wm.c
+	gcc wm.c -o wm -lxcb -lxcb-keysyms
---
+++ wm.c
@@ -0,0 +1,97 @@
+#include <stdlib.h>
+
+#include <xcb/xcb.h>
+#include <xcb/xcb-symbols.h>
+
+int i; /* used for loops */
+
+xcb_key_symbols_t *keysyms;
+
+typedef struct binding
+{
+	xcb_keysym_t key_sym;
+	xcb_keycode_t key_code;
+	uint16_t modifiers;
+	void (*function) (void);
+} binding;
+
+
+int main (void)
+{
+	xcb_connection_t *connection = xcb_connect(NULL, NULL);
+	const xcb_setup_t *setup = xcb_get_setup(connection);
+	xcb_window_t root = xcb_setup_roots_iterator(setup).data->root;
+	keysyms = xcb_key_symbols_alloc(connection);
+
+	int numbindings = 1;
+	binding bindings[numbindings];
+
+	/*
+	uint32_t window = xcb_generate_id(connection);
+	xcb_window_t *window = xcb_create_window(connection,
+						XCB_COPY_FROM_PARENT,
+						window,
+						root,
+						0,
+						0,
+						256,
+						256,
+						0,
+						NULL,
+						XCB_COPY_FROM_PARENT,
+						NULL);
+
+
+	xcb_map_window(connection, window);
+	*/
+
+	bindings[0].key_sym = 'r';
+	bindings[0].modifiers = XCB_MOD_MASK_CONTROL;
+	bindings[0].function = exec_dmenu;
+
+
+	for (i = 0; i < numbindings; i++)
+	{
+		bindings[i].key_code = key_sym_to_code(bindings[i].key_sym);
+		xcb_grab_key(connection, 1, root, bindings[i].modifiers, bindings[i].key_code, XCB_GRAB_MODE_ASYNC, XCB_GRAB_MODE_ASYNC);
+	}
+
+	xcb_flush(connection);
+
+	while (1)
+	{
+		xcb_generic_event_t *event = xcb_wait_for_event(connection);
+		xcb_key_press_event_t *key_event;
+		if (event->response_type & ~0x80 == XCB_KEY_PRESS)
+		{
+			key_event = (xcb_key_press_event_t *) event;
+			event->*function();
+		}
+	}
+}
+
+void exec_dmenu (void)
+{
+	system("exec dmenu_run");
+}
+
+
+xcb_keycode_t key_sym_to_code(xcb_keysym_t keysym)
+{
+    xcb_keycode_t *keyp;
+    xcb_keycode_t key;
+
+    /* We only use the first keysymbol, even if there are more. */
+    keyp = xcb_key_symbols_get_keycode(keysyms, keysym);
+
+    if (keyp == NULL)
+    {
+        return 0;
+    }
+
+    key = *keyp;
+    free(keyp);
+    
+    return key;
+}
+
